ded07bd3cb6b3684685679d62dbafbd1ae13282d
SJ <ujh253@gmail.com>
2018-12-05 13:27:34 +0900

    second commit -상지 오와아아아

--- Final/src/main/java/ticket/controller/admin/test1.java
+++ Final/src/main/java/ticket/controller/admin/test1.java
@@ -1,5 +1,14 @@
 package ticket.controller.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class test1 {
 
+	
+	@RequestMapping(value="/test")
+	public void test() {
+		
+	}
 }
