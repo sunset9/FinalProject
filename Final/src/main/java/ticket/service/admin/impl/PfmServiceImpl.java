2c7d411aaa722e2b554e106e5bee88fad013cea4
sunset9 <haejin940@gmail.com>
2018-12-05 14:07:09 +0900

    18.12.05 - Pfm 관련 컨트롤러, 서비스 생성 - 해진

---
+++ Final/src/main/java/ticket/service/admin/impl/PfmServiceImpl.java
@@ -0,0 +1,10 @@
+package ticket.service.admin.impl;
+
+import org.springframework.stereotype.Service;
+
+import ticket.service.admin.face.PfmService;
+
+@Service
+public class PfmServiceImpl implements PfmService{
+
+}
