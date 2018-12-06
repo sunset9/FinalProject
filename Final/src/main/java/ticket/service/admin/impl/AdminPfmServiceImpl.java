ee8dbe908432a280356260c6496629a71820a92f
sunset9 <haejin940@gmail.com>
2018-12-06 10:06:45 +0900

    18.12.06 공연 등록 부분 테스트 메소드 생성 - 해진

--- Final/src/main/java/ticket/service/admin/impl/AdminPfmServiceImpl.java
+++ Final/src/main/java/ticket/service/admin/impl/AdminPfmServiceImpl.java
@@ -1,10 +1,46 @@
 package ticket.service.admin.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ticket.dao.face.PfmDao;
+import ticket.dto.AgeGrade;
+import ticket.dto.Artist;
+import ticket.dto.Genre;
+import ticket.dto.Hall;
+import ticket.dto.Theme;
 import ticket.service.admin.face.AdminPfmService;
 
 @Service
 public class AdminPfmServiceImpl implements AdminPfmService{
+
+	@Autowired PfmDao pDao;
+	
+	@Override
+	public List<Genre> getGenreList() {
+		return pDao.selectAllGenre();
+	}
+
+	@Override
+	public List<Theme> getThemeList(Genre genre) {
+		return pDao.selectAllTheme(genre);
+	}
+
+	@Override
+	public List<AgeGrade> getAgeGradeList() {
+		return pDao.selectAllAgeGrade();
+	}
+
+	@Override
+	public List<Artist> getArtistList(Artist artist) {
+		return pDao.selectArtistByName(artist);
+	}
+
+	@Override
+	public List<Hall> getHallList() {
+		return pDao.selectAllHall();
+	}
 
 }
